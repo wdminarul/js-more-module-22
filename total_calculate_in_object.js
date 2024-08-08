22a600203019a4125f6a90c4abfc97b102eb9a0b
MD Minarul Islam <wdminarul>
2024-08-08 13:19:33 +0600

    total-calculate-from-object

--- total_calculate_in_object.js
+++ total_calculate_in_object.js
@@ -1,11 +1,11 @@
 const shopping =[
-    {name: 'Beef', price: 700},
-    {name: 'chicken', price: 250},
-    {name: 'potato', price: 50},
-    {name: 'vegetable', price: 80},
-    {name: 'oil', price: 230},
-    {name: 'Mosla', price: 150},
-    {name: 'rice', price: 1800},
+    {name: 'Beef', price: 700,quantity:1},
+    {name: 'chicken', price: 250, quantity:2},
+    {name: 'potato', price: 50, quantity:2},
+    {name: 'vegetable', price: 80, quantity:4},
+    {name: 'oil', price: 230, quantity:2},
+    {name: 'Mosla', price: 150, quantity:5},
+    {name: 'rice', price: 1800, quantity:30},
 ]
 
 function totalShoppingCost(prices){
@@ -18,4 +18,18 @@ function totalShoppingCost(prices){
 }
 
 const shoppingCost = totalShoppingCost(shopping);
-console.log('Total shopping cost is :', shoppingCost);
+// console.log('Total shopping cost is :', shoppingCost);
+
+
+function totalBazar(total){
+    let totalTaka = 0;
+    for(let num of total){
+        const multipleTotal = num.price * num.quantity;
+        totalTaka = totalTaka + multipleTotal;
+    }
+
+    return totalTaka
+}
+
+const totalKhoroc = totalBazar(shopping);
+console.log('This is total khoroc in bazar :', totalKhoroc)
